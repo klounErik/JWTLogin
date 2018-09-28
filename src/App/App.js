4f9b27d8ba5bb1dcb7920a423e05b970a76c0686
Jan Erik <janegge1990@gmail.com>
2018-09-28 22:33:10 +0200

    fixed path errors

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Login from './Components/Login'
-import Home from './Components/Home'
-import Profile from './Components/Profile'
-import Register from './Components/Register'
-import Logout from './Components/logout'
-import Nav from './Components/Nav'
+import Login from '../Components/Login/Login'
+import Home from '../Components/Home/Home'
+import Profile from '../Components/Profile/Profile'
+import Register from '../Components/Register/Register'
+import Logout from '../Components/Login/logout'
+import Nav from '../Components/Nav/Nav'
 import './App.css';
 
 class App extends Component {
